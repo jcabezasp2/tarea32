56fb8674865973193dd5401f9074bd3251ad3999
jcabe <jcabe@DESKTOP-75RMB14>
2021-12-26 18:07:31 +0100

    Programada la clase Battle, se supone que funciona.

--- src/tarea32/Battle.java
+++ src/tarea32/Battle.java
@@ -2,4 +2,94 @@ package tarea32;
 
 public class Battle {
 
+	public static void initBattle(Pokemon pokemon1, Pokemon pokemon2) {
+		
+	while(pokemon1.getHealth() != 0 && pokemon2.getHealth() != 0) {	
+		
+		if(pokemon1.getSpeed() > pokemon2.getSpeed()) {
+			
+			pokemon2.setHealth(pokemon2.getHealth() - pokemon1.getStrength());
+			narracionAtaque(pokemon1.getName(), pokemon2.getName(), pokemon1.getStrength(), pokemon2.getHealth());
+			
+			if(pokemon2.getHealth() > 0) {
+				
+				pokemon1.setHealth(pokemon1.getHealth() - pokemon2.getStrength());
+				narracionAtaque(pokemon2.getName(), pokemon1.getName(), pokemon2.getStrength(), pokemon1.getHealth());
+				
+			}else {
+				System.out.println(narracionPerdido(pokemon2.getName()));
+			}
+			
+			
+			
+		}else {
+		
+			pokemon1.setHealth(pokemon1.getHealth() - pokemon2.getStrength());
+			narracionAtaque(pokemon2.getName(), pokemon1.getName(), pokemon2.getStrength(), pokemon1.getHealth());
+			
+			if(pokemon1.getHealth() > 0) {
+				
+				pokemon2.setHealth(pokemon2.getHealth() - pokemon1.getStrength());
+				narracionAtaque(pokemon1.getName(), pokemon2.getName(), pokemon1.getStrength(), pokemon2.getHealth());
+				
+			}else {
+				
+				System.out.println(narracionPerdido(pokemon1.getName()));
+			}
+		}
+		
+		
+		
+		
+		
+			
+			
+		} //FIN DEL BUCLE
+	
+	
+
+
+	
+	
+	
+	
+	} // FIN DEL METODO INITBATTLE
+	
+	
+	
+	
+	
+	private static String narracionAtaque(String nombrePokemonAtaca, String nombrePokemonRecibe, int fuerza, int saludRestante ) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(nombrePokemonAtaca);
+		sb.append(" conmienza el ataque contra ");
+		sb.append(nombrePokemonRecibe);
+		sb.append("\n");
+		sb.append(nombrePokemonAtaca);
+		sb.append(" daña ");
+		sb.append(fuerza);
+		sb.append(" a ");
+		sb.append(nombrePokemonRecibe);
+		sb.append(" y a ");
+		sb.append(nombrePokemonRecibe);
+		sb.append(" le quedan ");
+		sb.append(saludRestante);
+		sb.append(" vida.\n");
+		sb.append("--------------------------------------------------------");
+		
+		return sb.toString();
+	}
+	
+	private static String narracionPerdido(String nombrePerdedor) {
+	
+		StringBuilder sb = new StringBuilder();
+	
+		sb.append(nombrePerdedor);
+		sb.append(" ha perdido el combate");
+		
+		return sb.toString();
+		
+	}
+	
+	
 }
--- src/tarea32/Pokemon.java
+++ src/tarea32/Pokemon.java
@@ -2,4 +2,56 @@ package tarea32;
 
 public class Pokemon {
 
+	private static final int INITIAL_HEALTH = 301;
+	private static final int INITIAL_STRENGTH = 51;
+	private static final int INITIAL_SPEED = 201;
+	private int health;
+	private int strength;
+	private int speed;
+	private String name;
+	
+	public Pokemon() {
+		this.health = (int)(Math.random() * INITIAL_HEALTH);
+		this.strength = (int)(Math.random() * INITIAL_STRENGTH);
+		this.speed = (int)(Math.random() * INITIAL_SPEED);
+		this.name = "";
+	}
+	
+	public Pokemon(int health, int strength, int speed, String name) {
+	
+		this.health = health;
+		this.strength = strength;
+		this.speed = speed;
+		this.name = name;
+		
+	}
+	
+	public int getHealth() {
+		return this.health;
+	}
+	
+	public int getStrength() {
+		return this.strength;
+	}
+	
+	public int getSpeed() {
+		return this.speed;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setHealth(int health) {
+	this.health = health;	
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+	
+	
 }
